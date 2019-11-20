97e327c400db91c3b6cefed8ead3b93c406fe878
elbrus07 <eolisaev@yandex.ru>
2019-11-20 15:51:21 +0300

    Скрипт создания таблиц БД

--- database/create_tbl.php
+++ database/create_tbl.php
@@ -1,21 +1,51 @@
 ﻿<?php
-set_time_limit(72000);
-
+  set_time_limit(72000);
   $dbd = cdb();
    mysqli_query($dbd,'drop database cms');
    mysqli_query($dbd,'create database cms');
    mysqli_query($dbd,'use cms');
 
-  $sql_str = 'create table ';
-  if(!mysqli_query($dbd,$sql_str)){
-      die('Ошибка создания таблицы autoinc '. mysqli_error($dbd));
-   } else{
-   
-   }
-   
-   
-   
-   
+// ***  Таблица РОЛЬ   
+  $str_sql = 'create table role (id mediumint unsigned auto_increment primary key, name char(100) default "", prim text, unique key(name))';
+  if(!mysqli_query($dbd,$str_sql)){
+    echo 'Ошибка создания таблицы role '. mysqli_error($dbd).'<br>';
+  }else{
+    if(!mysqli_query($dbd, 'insert into role (name) values ("администратор"), ("редактор"), ("автор")')){
+      echo 'Ошибка добавления в таблицу role '. mysqli_error($dbd).'<br>';    
+    }else{
+      echo 'Таблица role - создана!<br>';
+    }
+  }
+
+// ***  Таблица ПОЛЬЗОВАТЕЛЬ
+  $str_sql = 'create table user (id mediumint unsigned auto_increment primary key, role_id mediumint unsigned, fam char(70) default "",  name char(70) default "", otch char(70) default ""
+              , login char(100) default "", parol char(150) default "", e_mail char(100) default "", prim text,  unique key(login), key(role_id), key(fam,name,otch), key(e_mail)
+              , foreign key (role_id) references role(id) on delete restrict
+               )';
+  if(!mysqli_query($dbd,$str_sql)){
+    die('Ошибка создания таблицы user '. mysqli_error($dbd));
+  }else{
+    $parol = password_hash('1',PASSWORD_DEFAULT);
+    $str_sql = 'insert into user (login,parol,role_id,fam,name,otch, e_mail) values 
+                ("admin","'.$parol.'",1,"Иванов","Иван","Иванович","ddd@yandex.ru")
+                ,("redaktor","'.$parol.'",2,"Петров","Петр","Петрович","petrd@yandex.ru")
+                ,("avtor","' . $parol .'",3,"Зуев","Леонид","Гердович","zuid@yandex.ru")  ';
+    if(!mysqli_query($dbd,$str_sql)){
+      echo 'Ошибка добавления в таблицу user '. mysqli_error($dbd).'<br>';    
+    }else{
+      echo 'Таблица user - создана!<br>';
+    }
+  }
+
+// ***  Таблица КАТЕГОРИЯ СТАТЬИ
+
+
+// ***  Таблица СТАТЬЯ
+
+  
+
+  
+  
 // *** Функция соединения с БД   
 function cdb(){
   // Возвращает номер соединения или 0 (в случае неудачи)
